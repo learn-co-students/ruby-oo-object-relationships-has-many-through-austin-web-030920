60a096ece9c7ca9e2c285e7b10e70b70f4051a9b
github username <github email address>
2020-03-14 02:07:02 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
+require_relative 'meal'
 class Customer
-  
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        meal = Meal.new(waiter, self, total, tip)
+        # binding.pry
+        meal
+    end
+
+    def meals
+        Meal.all.select {|m| m.customer == self}
+    end
+
+    def waiters
+        meals.map {|m| m.waiter }.uniq
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,31 @@
-class Waiter
+require 'pry'
 
+class Waiter
+    @@all = []
+    def initialize(name, years)
+        @name = name
+        @years = years
+
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals 
+        Meal.all.select {|m| m.waiter == self }
+    end
+
+    def best_tipper
+        Meal.all.sort_by { |meal| meal.tip }.last.customer
+    end
 end
