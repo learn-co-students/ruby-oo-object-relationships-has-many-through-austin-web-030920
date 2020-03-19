979a4284853b93d02e7208ad8b73531cdeeee95d
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-03-18 22:37:38 -0500

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,22 +1,24 @@
-#CUSTOMER IS THE JOINER!!!
-
-
 class Meal
 
-    attr_accessor :waiter, :customer, :total, :tip
-  
-    @@all = []
-  
-    def initialize(waiter, customer, total, tip=0)
-      @waiter = waiter
-      @customer = customer
-      @total = total
-      @tip = tip
-      @@all << self
-    end
-  
-    def self.all
-      @@all
-    end
-  
+  attr_accessor :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
   end
+
+  def meals
+    Meal.all.select do |meal|
+      meal.customer == self
+    end
+  end
+
+  def self.all
+    @@all
+  end
+end
